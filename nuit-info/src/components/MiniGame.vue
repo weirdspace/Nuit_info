6dbf969b9268818186343c6f069125594686d092
fode <bdiakite00@gmail.com>
2023-12-08 05:07:46 +0100

    Word Finder Game

---
+++ nuit-info/src/components/MiniGame.vue
@@ -0,0 +1,33 @@
+
+<template>
+  <div class="alert alert-dismissible alert-warning">
+       <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+       <h4 class="alert-heading">Warning!</h4>
+        <p class="mb-0"> Le jeu va démarrer </p>
+        <p class="mb-0">Pour revenir en arrière cliquer sur :
+          <a href="/" class="alert-link">Retour sur les consignes</a> </p>
+  </div>
+</template>
+  
+  <script>
+
+ </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+  </style>
+  
